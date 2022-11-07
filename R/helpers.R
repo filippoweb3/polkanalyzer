b99bfb5e666d13bef241b5a098132c366a8ebbd6
Filippo <110459737+filippoweb3@users.noreply.github.com>
2022-11-07 08:40:50 +0100

    Added package files

---
+++ R/helpers.R
@@ -0,0 +1,134 @@
+fetch_data <- function(chain, era.interval){
+
+  first_era <- era.interval[1]
+
+  last_era <- era.interval[2]
+
+  era_difference <- last_era - first_era
+
+  eras <- list()
+
+  pb <- utils::txtProgressBar(min = 0, max = era_difference, style = 3)
+
+  for(i in 1:era_difference){
+
+    utils::setTxtProgressBar(pb, i)
+
+    era <- i + first_era
+
+    tab <- tryCatch(
+
+      read.csv(url(paste("https://storage.googleapis.com/watcher-csv-exporter/",
+                         chain, "_validators_era_", era, ".csv", sep = ""))),
+      error = function(e) e
+
+    )
+
+    if (inherits(tab, "error")) {
+
+      next
+
+    }
+
+    if(sum(colnames(tab) == "active") == 1){
+
+      tab <- tab[tab$active == 1,]
+
+    }
+
+    tab <- tab[,colnames(tab) %in% c("era", "session", "block_number", "name",
+                                     "commission_percent", "self_stake",
+                                     "total_stake", "num_stakers", "era_points")]
+
+    eras[[i]] <- tab
+
+  }
+
+  eras.clean <- eras[!unlist(lapply(eras, is.null))]
+
+  eras_data <- do.call(rbind, eras.clean)
+
+  return(list(eras = eras_data, chain = chain, interval = era.interval))
+
+}
+
+update_data <- function(data, era){
+
+  chain <- data$chain
+
+  max_era <- data$interval[2]
+
+  if(era == max_era){
+
+    print("Dataset up to date.")
+
+    return(data)
+
+  } else {
+
+    start <- max_era + 1
+
+    new_eras <- fetch_data(chain = chain, era.interval = c(start, era))
+
+    updated <- list(
+
+      eras = rbind(data, new_eras$eras),
+      chain = chain,
+      interval = c(data$interval[1], era)
+
+    )
+
+    return(updated)
+
+  }
+
+}
+
+plot_data <- function(data, validator.name, look.back = 80){
+
+  max.era <- max(data$era)
+
+  min.era <- max.era - look.back
+
+  filter <- data$name %in% validator.name
+
+  val.data <- data[filter,]
+
+  val.data <- subset(val.data, era >= min.era)
+
+  par(mfrow=c(2,2))
+
+  plot(val.data$era, val.data$era_points, type ="b", xlab = "Eras", ylab = "Era Points", ylim = c(0, 90000))
+
+  plot(val.data$era, val.data$self_stake/10^10, type ="b", xlab = "Eras", ylab = "Self Stake")
+
+  plot(val.data$era, val.data$total_stake/10^10, type ="b", xlab = "Eras", ylab = "Total Stake")
+
+  plot(val.data$era, val.data$commission_percent, type ="b", xlab = "Eras", ylab = "Commission (%)")
+
+}
+
+select_validator <- function(data, look.back = 80, criteria){
+
+  last_era <- max(data$eras$era)
+
+  sub <- subset(data$eras,era > (last_era - look.back))
+
+  sum <- data.frame(group_by(sub, name) %>%
+                      summarise(sum(era_points > 50000)/length(era_points),
+                                n = length(era_points),
+                                comm = mean(commission_percent),
+                                ss = mean(self_stake)/10^10,
+                                ts = mean(total_stake)/10^10))
+
+  colnames(sum) <- c("name","pct", "n", "m_comm", "m_self", "m_total")
+
+  selection <- subset(sum, m_self > criteria$self &
+                        m_total < criteria$total &
+                        pct > criteria$pct & pct < 1 &
+                        m_comm <= criteria$comm &
+                        n >= criteria$n)
+
+  return(selection)
+
+}
---
+++ Scripts/raw_script.R
@@ -0,0 +1,63 @@
+library(dplyr)
+
+# Load data ----
+
+#eras_data <- fetch_data(chain = "polkadot", era.interval = c(165, 886))
+
+usethis::use_data(eras_data, overwrite = T)
+
+# Update data ----
+
+eras_data <- update_data(data = eras_data, era = 886)
+
+usethis::use_data(eras_data, overwrite = T)
+
+
+selection <- select_validator(data = eras_data, look.back = 80,
+                 criteria = list(pct = 0.6,
+                                 self = 5000,
+                                 total = 2200000,
+                                 comm = 5,
+                                 n = 10))
+
+val_names <- selection$name
+
+plot_data(data = eras_data$eras, val_names[1])
+
+
+val_names <- unique(eras_data$name)
+val_names <- val_names[!val_names == ""]
+val_name <- val_names[order(val_names)]
+val_ID <- c(1:length(val_name))
+
+
+
+
+
+
+
+# Plots ----
+
+pct_less_100_comm <- group_by(eras_data$eras, era) %>% summarise(sum(commission_percent < 100)/length(commission_percent)*100)
+
+plot(pct_less_100_comm, xlab = "Eras", ylab = "Pct Valitators < 100% comm", type = "l")
+
+
+
+
+for(i in 1:length(val_names)){
+
+  sub_data <- subset(eras_data, name == val_names[i])
+
+  col <- ifelse(sub_data$era_points <= 40000, "red", ifelse(sub_data$era_points <= 60000 & sub_data$era_points > 40000, "orange", "green"))
+
+  if(i == 1){
+
+    plot(sub_data$era, rep(i, length(sub_data$era)), col = col, pch = 19, cex = 0.5, xlim = c(750, last_era), ylim =c(1,length(val_name)))
+
+  }
+
+  abline(h = i, col = "grey", lwd = 0.2)
+  points(sub_data$era, rep(i, length(sub_data$era)), col = col, pch = 19, cex = 0.5)
+
+}
