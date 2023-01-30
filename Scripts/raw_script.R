925060ba00a1ba30076807a3a76f7ff55d8f6489
Filippo <filippo@web3.foundation>
2023-01-30 17:51:50 +0100

    removed digital ocean as provider, minor edits

--- Scripts/raw_script.R
+++ Scripts/raw_script.R
@@ -12,7 +12,7 @@ usethis::use_data(candidates, overwrite = T)
 
 # Update watcher data ----
 
-eras_data <- update_watcher_data(data = eras_data, era = 968)
+eras_data <- update_watcher_data(data = eras_data, era = 972)
 
 usethis::use_data(eras_data, overwrite = T)
 
@@ -20,7 +20,7 @@ usethis::use_data(eras_data, overwrite = T)
 
 selection <- select_validator(data = eras_data, look.back = 30,
                               criteria = list(pct = 0.6,
-                                              self_stake = 6500,
+                                              self_stake = 6800,
                                               total_stake = 1800000,
                                               commission = 5,
                                               n_active = 30,
@@ -29,7 +29,8 @@ selection <- select_validator(data = eras_data, look.back = 30,
 
 selection <- merge(selection, candidates, by = "stash_address")
 
-selection <- selection[!selection$provider == "Hetzner Online GmbH" &
+selection <- selection[!selection$provider %in% c("Hetzner Online GmbH",
+                                                "DigitalOcean, LLC") &
                          selection$id_verified == TRUE &
                          selection$democracyVoteCount >= 1 &
                          selection$councilVoteCount >= 1 &
