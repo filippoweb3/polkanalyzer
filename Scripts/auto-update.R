4f211ac5783d072746d7f5822e83ab91550d7d6e
filippoweb3 <filippo@web3.foundation>
2023-02-25 18:15:59 +0100

    fix

--- Scripts/auto-update.R
+++ Scripts/auto-update.R
@@ -1,13 +1,9 @@
 library(rjson)
 library(dplyr)
 
-devtools::load_all(".")
-
-load("data/eras_data.rda")
 load("data/candidates.rda")
 
-candidates <- fetch_candidates()
-usethis::use_data(candidates, overwrite = T)
+candidates1 <- candidates
 
-eras_data <- update_watcher_data(data = eras_data, era = 997)
-usethis::use_data(eras_data, overwrite = T)
+usethis::use_data(candidates1, overwrite = T)
+
