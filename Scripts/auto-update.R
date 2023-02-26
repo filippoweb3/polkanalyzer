5ed192758aff25ca06336307a82701dd5bd53808
actions-user <filippo@web3.foundation>
2023-02-26 16:23:32 +0100

    fix

--- Scripts/auto-update.R
+++ Scripts/auto-update.R
@@ -8,3 +8,13 @@ candidates <- fetch_candidates()
 
 usethis::use_data(candidates, overwrite = T)
 
+load("./data/eras_data.rda")
+
+diff <- (Sys.Date() - 1) - as.Date("2020-06-02",format="%Y-%m-%d")
+
+eras_data <- update_watcher_data(data = eras_data, era = diff)
+
+usethis::use_data(eras_data, overwrite = T)
+
+
+
